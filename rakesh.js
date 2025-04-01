9a66e16ed2729139795290a7fa2f0d2114eaaa69
Rakesh Pavan <110276813+rakeshtadi@users.noreply.github.com>
2025-04-01 12:38:55 +0530

    Add files via upload

--- rakesh.js
+++ rakesh.js
@@ -1,14 +1,5 @@
-let sum = 0;
-for (let i=1; i<=5; i++){
-    sum = sum+i;
-    
+let i = 1;
+while(i<=5){
+    console.log("i = ",i)
+    i++;
 }
-console.log(sum);
-console.log("loop has ended")
-
-
-for (let i = 1; i <= 5; i++) {  // The semicolon ends the loop
-    console.log("i = ", i);      // This block doesn't run with the loop
-}
-let i = 5;                       // Re-declares `i` as 5
-console.log(i);                  // Logs the new value of `i`
